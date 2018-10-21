f8a5f044d2cd2ec35170bdfe1e64e6b151710bf5
Damo <27757251@qq.com>
2018-10-22 01:36:12 +0800

    Add some classes data

---
+++ datas/Enum.lua
@@ -0,0 +1,20 @@
+--武器类型
+WeaponType={
+	"Sword",--剑
+	"Spear",--枪
+	"Axe",--斧
+	"Bow",--弓
+	"Magic",--魔法
+	"Wand"--杖
+}
+
+Attribute={
+	"None",--无属性
+	"Wind",--风
+	"Ice",--冰
+	"Fire",--火
+	"Thunder",--雷
+	"Physic",--理
+	"Light",--光
+	"Dark"--暗
+}
